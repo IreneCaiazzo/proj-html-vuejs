110e5b3ec49c103ac4c94f0ef8bb4c2f40a3ebff
IreneCaiazzo <irene.caiazzo707@gmail.com>
2023-05-30 12:43:38 +0200

    add data

---
+++ data.js
@@ -0,0 +1,175 @@
+const sec1 = [
+    {
+        id: 1,
+        title: 'WHO WE ARE',
+        paragraph: 'Quae molestiae, mollitia repellat aut accusamus debitis deleniti nemo cum vitae voluptates dicta dolores nihil aperiam minus!',
+        button: {
+            text: 'LEARN MORE',
+        }
+    },
+    {
+        id: 2,
+        title: 'WHAT WE DO',
+        paragraph: 'Quae molestiae, mollitia repellat aut accusamus debitis deleniti nemo cum vitae voluptates dicta dolores nihil aperiam minus!',
+        button: {
+            text: 'LEARN MORE',
+        }
+    },
+    {
+        id: 3,
+        title: 'WHERE WE WORK',
+        paragraph: 'Quae molestiae, mollitia repellat aut accusamus debitis deleniti nemo cum vitae voluptates dicta dolores nihil aperiam minus!',
+        button: {
+            text: 'LEARN MORE',
+        }
+    },
+];
+
+const sec2 = [
+    {
+        id: 1,
+        icon: '<font-awesome-icon icon="fa-solid fa-check" />',
+        title: 'A GLOBAL VIEW',
+        paragraph: 'Quae molestiae, mollitia repellat aut accusamus debitis deleniti nemo cum vitae voluptates dicta dolores nihil aperiam minus!'
+    },
+    {
+        id: 2,
+        icon: '<font-awesome-icon icon="fa-solid fa-check" />',
+        title: 'EXPERT LAWYERS',
+        paragraph: 'Quae molestiae, mollitia repellat aut accusamus debitis deleniti nemo cum vitae voluptates dicta dolores nihil aperiam minus!'
+    },
+    {
+        id: 3,
+        icon: '<font-awesome-icon icon="fa-solid fa-check" />',
+        title: 'CLIENT FOCUS',
+        paragraph: 'Quae molestiae, mollitia repellat aut accusamus debitis deleniti nemo cum vitae voluptates dicta dolores nihil aperiam minus!'
+    },
+    {
+        id: 4,
+        icon: '<font-awesome-icon icon="fa-solid fa-check" />',
+        title: 'LOCAL KNOWLEDGE',
+        paragraph: 'Quae molestiae, mollitia repellat aut accusamus debitis deleniti nemo cum vitae voluptates dicta dolores nihil aperiam minus!'
+    },
+];
+
+const ourPeople = [
+    {
+        id: 1,
+        img: 'img/column1-image1.jpg',
+        name: 'Deborah Brown',
+    },
+    {
+        id: 2,
+        img: 'img/column1-image2.jpg',
+        name: 'Allen Lucas',
+    },
+    {
+        id: 3,
+        img: 'img/column1-image3.jpg',
+        name: 'Muriel Potter',
+    },
+    {
+        id: 4,
+        img: 'img/column1-image4.jpg',
+        name: 'Ken Douglas',
+    },
+    {
+        id: 5,
+        img: 'img/column1-image5.jpg',
+        name: 'Loretta Harris',
+    },
+    {
+        id: 6,
+        img: 'img/column1-image6.jpg',
+        name: 'Phil Russell',
+    },
+];
+
+const ourPracticeAreas = [
+    {
+        id: 1,
+        img: 'img/column2-image1.jpg',
+        name: 'Tax Litigation',
+    },
+    {
+        id: 2,
+        img: 'img/column2-image2.jpg',
+        name: 'Acquisitions',
+    },
+    {
+        id: 3,
+        img: 'img/column2-image3.jpg',
+        name: 'Financial Advice',
+    },
+    {
+        id: 4,
+        img: 'img/column2-image4.jpg',
+        name: 'International',
+    },
+    {
+        id: 5,
+        img: 'img/column2-image5.jpg',
+        name: 'Oil and Gas',
+    },
+    {
+        id: 6,
+        img: 'img/column2-image6.jpg',
+        name: 'Capital Markets',
+    },
+];
+
+const ourPublications = [
+    {
+        id: 1,
+        img: 'img/column3-image1.jpg',
+        name: 'Low Oil Prices',
+    },
+    {
+        id: 2,
+        img: 'img/column3-image2.jpg',
+        name: 'Cost of Divorce',
+    },
+    {
+        id: 3,
+        img: 'img/column3-image3.jpg',
+        name: '2015 Tax Laws',
+    },
+    {
+        id: 4,
+        img: 'img/column3-image4.jpg',
+        name: 'Surveys Matter',
+    },
+    {
+        id: 5,
+        img: 'img/column3-image5.jpg',
+        name: 'In-House Work',
+    },
+    {
+        id: 6,
+        img: 'img/column3-image6.jpg',
+        name: 'Obama Care',
+    },
+];
+
+const sec6 = [
+    {
+        id: 1,
+        img: 'img/law_7.jpg',
+        title: 'INTERNATIONAL',
+        paragraph: 'Quae molestiae, mollitia repellat aut accusamus debitis deleniti nemo cum vitae voluptates dicta dolores nihil aperiam minus!',
+    },
+    {
+        id: 2,
+        img: 'img/law_8.jpg',
+        title: 'PARTNERSHIP',
+        paragraph: 'Quae molestiae, mollitia repellat aut accusamus debitis deleniti nemo cum vitae voluptates dicta dolores nihil aperiam minus!',
+    },
+    {
+        id: 3,
+        img: 'img/law_3.jpg',
+        title: 'EXCELLENCE',
+        paragraph: 'Quae molestiae, mollitia repellat aut accusamus debitis deleniti nemo cum vitae voluptates dicta dolores nihil aperiam minus!',
+    },
+];
+
+export default sec1; sec2; ourPeople; ourPracticeAreas; ourPublications; sec6;
