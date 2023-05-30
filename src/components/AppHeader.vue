c5abc895ba79ae05a88f7224041fda7638cd837e
IreneCaiazzo <irene.caiazzo707@gmail.com>
2023-05-30 15:30:57 +0200

    tryout header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,12 +2,40 @@
 
 </script>
 
-<template></template>
+<template>
+    <header>
+        <div class="logo">
+            <img src="../../img/avada-law-logo.png" alt="">
+        </div>
+
+        <div class="navbar">
+            <h2>Home</h2>
+            <h2>Who We Are</h2>
+            <h2>What We Do</h2>
+            <h2>Where We Work</h2>
+            <h2>Careers</h2>
+            <h2>News</h2>
+            <h2>Contact</h2>
+        </div>
+    </header>
+</template>
 
 <style scoped>
 * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+}
+
+header {
+    display: flex;
+    justify-content: space-between;
+    width: 1200px;
+}
+
+img {
+    width: 100px;
+    height: 100px;
+
 }
 </style>
