97185da1d4bfd9839fff03c75892b9a629a317ff
IreneCaiazzo <irene.caiazzo707@gmail.com>
2023-05-30 14:48:38 +0200

    import bootstrap

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+/* import Bootstrap library */
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap';
+
 /* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core'
 
