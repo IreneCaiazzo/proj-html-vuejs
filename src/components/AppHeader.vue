6cf172dd49517d3d3b399d7441cdf66a68bf5a33
IreneCaiazzo <irene.caiazzo707@gmail.com>
2023-05-31 17:59:30 +0200

    fixed header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,30 +6,34 @@
     <header>
 
         <div class="container">
-            <span class="company">Avada & Associates legal team announce deal to open up oil & gas</span>
+            <div class="subcontainer">
 
-            <div class="social">
-                <font-awesome-icon icon="fa-brands fa-square-facebook" />
-                <font-awesome-icon icon="fa-brands fa-square-twitter" />
-                <font-awesome-icon icon="fa-brands fa-square-instagram" />
-                <font-awesome-icon icon="fa-brands fa-square-youtube" />
-            </div>
-        </div>
+                <span class="company">Avada & Associates legal team announce deal to open up oil & gas</span>
 
-        <div class="container-2">
-            <div class="logo">
-                <img src="../../img/avada-law-logo.png" alt="">
+                <div class="social">
+                    <font-awesome-icon icon="fa-brands fa-square-facebook" />
+                    <font-awesome-icon icon="fa-brands fa-square-twitter" />
+                    <font-awesome-icon icon="fa-brands fa-square-instagram" />
+                    <font-awesome-icon icon="fa-brands fa-square-youtube" />
+                </div>
             </div>
 
-            <ul class="navbar">
-                <li>Home</li>
-                <li>Who We Are</li>
-                <li>What We Do</li>
-                <li>Where We Work</li>
-                <li>Careers</li>
-                <li>News</li>
-                <li>Contact</li>
-            </ul>
+
+            <div class="container-2">
+                <div class="logo">
+                    <img src="../../img/avada-law-logo.png" alt="">
+                </div>
+
+                <ul class="navbar">
+                    <li>Home</li>
+                    <li>Who We Are</li>
+                    <li>What We Do</li>
+                    <li>Where We Work</li>
+                    <li>Careers</li>
+                    <li>News</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
         </div>
 
         <div class="jumbo">
@@ -51,9 +55,21 @@ header {
     top: 0; */
     width: 100%;
     margin: 0 auto;
+    background-color: white;
 }
 
 header .container {
+    width: 100%;
+    height: 8rem;
+    background-color: white;
+    position: fixed;
+    top: 0;
+    right: 0;
+    left: 0;
+    z-index: 2;
+}
+
+header .subcontainer {
     display: flex;
     justify-content: space-between;
     width: 1200px;
@@ -79,8 +95,13 @@ header .container-2 {
     display: flex;
     justify-content: space-between;
     align-items: center;
+    position: fixed;
+    top: 0;
+    right: 0;
+    left: 0;
+    z-index: 2;
     width: 1200px;
-    margin: 0 auto;
+    margin: 1rem auto;
     padding: 2rem 0;
 }
 
@@ -94,5 +115,6 @@ header .jumbo img {
     height: 20rem;
     display: block;
     object-fit: cover;
+    margin-top: 8rem;
 }
 </style>
